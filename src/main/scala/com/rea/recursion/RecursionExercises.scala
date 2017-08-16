901bb0911aa40c11a0e347cd203dd5e22ecbb405
Ben Theunissen <ben.theunissen@rea-group.com>
2017-08-16 15:13:04 +1000

    feat: refactored early solutions to use pattern matching, tail recursion where evident

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -1,5 +1,7 @@
 package com.rea.recursion
 
+import scala.annotation.tailrec
+
 // Taken from http://tmorris.net/posts/scala-exercises-for-beginners/index.html
 
 /**
@@ -30,63 +32,51 @@ object RecursionExercises {
 
   // Add two non-negative Integers together.  You are only allowed to use plusOne and minusOne above
   def add(a: Int, b: Int): Int = {
-    if (b == 0) {
-      a
-    } else {
-      add(plusOne(a), minusOne(b))
+    b match {
+      case 0 => a
+      case _ => add(plusOne(a), minusOne(b))
     }
   }
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
   def sum(l: List[Int]): Int = {
-    if(l.isEmpty) {
-      0
-    } else {
-      l.head + sum(l.tail)
+    @tailrec
+    def sumHelper(l: List[Int], currentSum: Int): Int = l match {
+      case Nil => currentSum
+      case _ => sumHelper(l.tail, currentSum + l.head)
     }
+    sumHelper(l, 0)
   }
 
   //Again no list functions are permitted for the following
   def length[A](x: List[A]): Int = {
-    if(x.isEmpty) {
-      0
-    } else {
-      1 + length(x.tail)
+    @tailrec
+    def lengthHelper(x: List[A], currentLength: Int): Int = x match {
+      case Nil => currentLength
+      case _ => lengthHelper(x.tail, 1 + currentLength)
     }
+    lengthHelper(x, 0)
   }
 
   // Do you notice anything similar between sum and length? Hmm...
 
   // Mapping over a list.  You are given a List of type A and a function converting an A to a B
   // and you give back a list of type B.  No list functions allowed!
-  def map[A, B](x: List[A], f: A => B): List[B] = {
-    if(x.isEmpty){
-      Nil
-    } else {
-      f(x.head)::map(x.tail, f)
-    }
+  def map[A, B](x: List[A], f: A => B): List[B] = x match {
+    case Nil => Nil
+    case _ => f(x.head)::map(x.tail, f)
   }
 
   // Given a function from A => Boolean, return a list with only those item where the function returned true.
-  def filter[A](x: List[A], f: A => Boolean): List[A] = {
-    if(x.isEmpty){
-      x
-    } else if(f(x.head)) {
-      x.head::filter(x.tail, f)
-    } else {
-      filter(x.tail, f)
-    }
+  def filter[A](x: List[A], f: A => Boolean): List[A] = x match {
+    case Nil => Nil
+    case _ => if (f(x.head)) x.head::filter(x.tail, f) else filter(x.tail, f)
   }
 
   // This pattern should be familiar by now... psst... look at add.
-  def append[A](x: List[A], y: List[A]): List[A] = {
-    if(x.nonEmpty) {
-      x.head::append(x.tail, y)
-    } else if(y.nonEmpty) {
-      y.head::append(x, y.tail)
-    } else {
-      Nil
-    }
+  def append[A](x: List[A], y: List[A]): List[A] = x match {
+    case Nil => if(y.nonEmpty) y.head::append(x, y.tail) else Nil
+    case _ => x.head::append(x.tail, y)
   }
 
   // Flatten a list of lists to a single list.  Remember you can't use list.flatten.  Can you use a previous
@@ -112,11 +102,11 @@ object RecursionExercises {
 
   // Maximum of the empty list is 0
   def maximum(x: List[Int]): Int = {
-    if(x.isEmpty) {
-      0
-    } else {
-      Math.max(x.head, maximum(x.tail))
+    def maximumHelper(x: List[Int], runningMax: Int) : Int = x match {
+      case Nil => runningMax
+      case _ => maximumHelper(x.tail, Math.max(x.head, runningMax))
     }
+    maximumHelper(x, 0)
   }
 
   // Reverse a list
